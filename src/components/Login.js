6a830ce85dbb8eacb97ad7125a09a76a587e627d
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-10-01 14:24:57 -0400

    add <Login /> with form functionality

---
+++ src/components/Login.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { userLogin } from "./../actions/creators";
+
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleInputChange = e => {
+    let key = e.target.name;
+    let value = e.target.value;
+    let updateStateWith = {};
+    updateStateWith[key] = value;
+    this.setState(updateStateWith);
+  };
+
+  handleFormSubmission = e => {
+    e.preventDefault();
+    console.log("handleFormSubmission: ", this.state);
+    this.props.handleLogin(this.state);
+  };
+
+  render() {
+    let { username, password } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmission}>
+          <label>
+            Username:
+            <input
+              onChange={this.handleInputChange}
+              type="text"
+              name="username"
+              value={username}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              onChange={this.handleInputChange}
+              type="password"
+              name="password"
+              value={password}
+            />
+          </label>
+          <input type="submit" value="login" />
+        </form>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    handleLogin: formData => dispatch(userLogin(formData))
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Login);
