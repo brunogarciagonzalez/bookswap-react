81f3ec720bd4e263f91fb78fa8cfe905d35b66d4
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-10-09 21:29:39 -0400

    include redirectToReducer in rootReducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import redirectToReducer from "./redirectToReducer.js";
 import loginFormReducer from "./loginFormReducer.js";
 import userReducer from "./userReducer.js";
 import addUserBookFormReducer from "./addUserBookFormReducer.js";
@@ -6,6 +7,7 @@ import selectedUserBookReducer from "./selectedUserBookReducer.js";
 import booksReducer from "./booksReducer.js";
 
 const rootReducer = combineReducers({
+  redirectTo: redirectToReducer,
   loginForm: loginFormReducer,
   user: userReducer,
   addUserBookForm: addUserBookFormReducer,
